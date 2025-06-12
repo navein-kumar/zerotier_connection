d403d3cc4c40862612b3a363d46d9ee7e6363f30
Naveen-Kumar <30574883+navein-kumar@users.noreply.github.com>
2025-06-12 11:40:58 +0530

    Update ip_forward.sh

--- ip_forward.sh
+++ ip_forward.sh
@@ -1,4 +1,4 @@
---Ubuntu----
+---------Ubuntu------------------------------------------------------
 vim /etc/sysctl.conf
 #out 
 net.ipv4.ip_forward = 1
@@ -22,9 +22,12 @@ sudo iptables -A FORWARD -i ztliuxnmeq -o ens18 -j ACCEPT
 sudo apt install iptables-persistent
 sudo systemctl enable iptables
 sudo iptables-save > /etc/iptables/rules.v4
-----------------------
---kali below---
----------------------
+----------------------end-ubuntu--------------------------------------------------
+
+
+
+--kali-below--------------------------------------------------
+---------------------step-1
 echo buster | sudo tee /etc/debian_version >/dev/null
 curl -s https://install.zerotier.com | sudo bash
 echo $DV_SAVE | sudo tee /etc/debian_version >/dev/null
@@ -35,8 +38,8 @@ sudo iptables -A INPUT -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -i ztliuubgwp -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o ztliuubgwp -j ACCEPT
 sudo iptables -A FORWARD -i ztliuubgwp -o eth0 -j ACCEPT
---------------------------------------------------------
-save reboot ----
+-----step---2----perstitnace-------
+save reboot 
 sudo nano /etc/rc.local
 
 #!/bin/bash
@@ -52,7 +55,7 @@ sudo chmod +x /etc/rc.local
 
 sudo systemctl stop apparmor && sudo systemctl disable apparmor
 
---
+-----step-3-----------------------
 cat /proc/sys/net/ipv4/ip_forward = 1
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 
@@ -62,7 +65,12 @@ sudo sysctl -p
 sudo nano /etc/sysctl.d/99-custom.conf
 net.ipv4.ip_forward=1
 sudo sysctl --system
+-------------------kali--end-----------------
 
+
+
+
+------fast-direct-connect-----------------
 -------------------------------------------------------------------
 #tunnel only to moons
 sudo iptables -A OUTPUT -p udp --dport 9993 -j DROP
